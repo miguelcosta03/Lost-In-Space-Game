55075e3bafac5705a37d33b778aec20ad4d0e9b7
my5er <66944221+my5er@users.noreply.github.com>
2020-07-23 16:41:04 +0100

    Levels 1 and 2 development is finally complete!

--- MainScript.py
+++ MainScript.py
@@ -30,125 +30,43 @@ def start_click():
                     if try_password == try_password_click:
                         c_random_password = [char for char in lp]
                         password_string = ''.join(map(str, c_random_password))  # Convert the ll list to string
-
-                        letter_a_position = password_string.find('a')
-                        letter_b_position = password_string.find('b')
-                        letter_c_position = password_string.find('c')
-                        letter_d_position = password_string.find('d')
-                        letter_e_position = password_string.find('e')
-                        letter_f_position = password_string.find('f')
-                        letter_g_position = password_string.find('g')
-                        letter_h_position = password_string.find('h')
-                        letter_i_position = password_string.find('i')
-                        letter_j_position = password_string.find('j')
-                        letter_k_position = password_string.find('k')
-                        letter_l_position = password_string.find('l')
-                        letter_m_position = password_string.find('m')
-                        letter_n_position = password_string.find('n')
-                        letter_o_position = password_string.find('o')
-                        letter_p_position = password_string.find('p')
-                        letter_q_position = password_string.find('q')
-                        letter_r_position = password_string.find('r')
-                        letter_s_position = password_string.find('s')
-                        letter_t_position = password_string.find('t')
-                        letter_u_position = password_string.find('u')
-                        letter_v_position = password_string.find('v')
-                        letter_w_position = password_string.find('w')
-                        letter_x_position = password_string.find('x')
-                        letter_y_position = password_string.find('y')
-                        letter_z_position = password_string.find('z')
-
-                        # Position of letters in password attempt
-                        c_password_attempt = [char for char in p]
-                        password_attempt = ''.join(map(str, c_password_attempt))
-                        letter_a_password_attempt_position = password_attempt.find('a')
-                        letter_b_password_attempt_position = password_attempt.find('b')
-                        letter_c_password_attempt_position = password_attempt.find('c')
-                        letter_d_password_attempt_position = password_attempt.find('d')
-                        letter_e_password_attempt_position = password_attempt.find('e')
-                        letter_f_password_attempt_position = password_attempt.find('f')
-                        letter_g_password_attempt_position = password_attempt.find('g')
-                        letter_h_password_attempt_position = password_attempt.find('h')
-                        letter_i_password_attempt_position = password_attempt.find('i')
-                        letter_j_password_attempt_position = password_attempt.find('j')
-                        letter_k_password_attempt_position = password_attempt.find('k')
-                        letter_l_password_attempt_position = password_attempt.find('l')
-                        letter_m_password_attempt_position = password_attempt.find('m')
-                        letter_n_password_attempt_position = password_attempt.find('n')
-                        letter_o_password_attempt_position = password_attempt.find('o')
-                        letter_p_password_attempt_position = password_attempt.find('p')
-                        letter_q_password_attempt_position = password_attempt.find('q')
-                        letter_r_password_attempt_position = password_attempt.find('r')
-                        letter_s_password_attempt_position = password_attempt.find('s')
-                        letter_t_password_attempt_position = password_attempt.find('t')
-                        letter_u_password_attempt_position = password_attempt.find('u')
-                        letter_v_password_attempt_position = password_attempt.find('v')
-                        letter_w_password_attempt_position = password_attempt.find('w')
-                        letter_x_password_attempt_position = password_attempt.find('x')
-                        letter_y_password_attempt_position = password_attempt.find('y')
-                        letter_z_password_attempt_position = password_attempt.find('z')
-
                         if random_password == 'space':
-                            lb_s['text'] = 'This password contains 5 letters. 2-Vowels and 3-Consoants'
-                            if letter_s_position == letter_s_password_attempt_position:
-                                lb_attempt['text'] = 'First Letter Succesfully Cracked'
-                                if letter_p_position == letter_p_password_attempt_position:
-                                    lb_attempt['text'] = 'Second Letter Succesfully Cracked'
-                                    if letter_a_position == letter_a_password_attempt_position:
-                                        lb_attempt['text'] = 'Third Letter Succesfully Cracked'
-                                        if letter_c_position == letter_c_password_attempt_position:
-                                            lb_attempt['text'] = 'Fourth Letter Succesfully Cracked'
-                                            if letter_e_position == letter_e_password_attempt_position:
-                                                lb_attempt['text'] = 'Fifth Letter Succesfully Cracked! Password Cracked'
-                                            else:
-                                                lb_attempt['text'] = 'Try again!'
-                                        else:
-                                            lb_attempt['text'] = 'Try again!'
-                                    else:
-                                        lb_attempt['text'] = 'Try again!'
-                                else:
-                                    lb_attempt['text'] = 'Try again!'
+                            if p == 'space':
+                                lb_attempt['text'] = 'Password Succesfully Cracked!'
                             else:
-                                lb_attempt['text'] = 'Try again!'
+                                lb_attempt['text'] = 'Password Not Cracked!'
 
-                        elif random_password == 'ship':
-                            lb_s['text'] = 'This password contains 4 letters. 1-Vowel and 3-Consoants'
-                            if letter_s_position == letter_s_password_attempt_position:
-                                lb_attempt['text'] = 'First Letter Succesfully Cracked'
-                                if letter_h_position == letter_h_password_attempt_position:
-                                    lb_attempt['text'] = 'Second Letter Succesfully Cracked'
-                                    if letter_i_position == letter_i_password_attempt_position:
-                                        lb_attempt['text'] = 'Third Letter Succesfully Cracked'
-                                        if letter_p_position == letter_p_password_attempt_position:
-                                            lb_attempt['text'] = 'Fourth Letter Succesfully Cracked. Password Cracked'
-                                        else:
-                                            lb_attempt['text'] = 'Try again!'
-                                    else:
-                                        lb_attempt['text'] = 'Try again!'
-                                else:
-                                    lb_attempt['text'] = 'Try again!'
+                        if random_password == 'ship':
+                            if p == 'ship':
+                                lb_attempt['text'] = 'Password Succesfully Cracked!'
                             else:
-                                lb_attempt['text'] = 'Try again!'
+                                lb_attempt['text'] = 'Password Not Cracked!'
 
-                        elif random_password == 'star':
-                            lb_s['text'] = 'This password contains 4 letters. 1-Vowel and 3-Consoants'
-                            if letter_s_position == letter_s_password_attempt_position:
-                                lb_attempt['text'] = 'First Letter Succesfully Cracked'
-                                if letter_t_position == letter_t_password_attempt_position:
-                                    lb_attempt['text'] = 'Second Letter Succesfully Cracked'
-                                    if letter_a_position == letter_a_password_attempt_position:
-                                        lb_attempt['text'] = 'Third Letter Succesfully Cracked'
-                                        if letter_a_position == letter_a_password_attempt_position:
-                                            lb_attempt['text'] = 'Fourth Letter Succesfully Cracked. Password Cracked!'
-                                        else:
-                                            lb_attempt['text'] = 'Try again!'
-                                    else:
-                                        lb_attempt['text'] = 'Try again!'
-                                else:
-                                    lb_attempt['text'] = 'Try again!'
+                        if random_password == 'star':
+                            if p == 'star':
+                                lb_attempt['text'] = 'Password Succesfully Cracked!'
                             else:
-                                lb_attempt['text'] = 'Try again!'
+                                lb_attempt['text'] = 'Password Not Cracked!'
+
+                def possible_passwords_click():
+                    possible_passwords = possible_passwords_click
+                    if possible_passwords == possible_passwords_click:
+                        lb_possible_passwords_list['text'] = 'space \nship \nstar'
+
+                def tip_click():
+                    tip = tip_click
+                    if tip == tip_click and random_password == 'space':
+                        lb_tip[
+                            'text'] = "This password contains 5 letters. 2-Vowels and 3-Consonants. That's where everything is "
+                    elif tip == tip_click and random_password == 'spaceship':
+                        lb_tip['text'] = "This password contains 9 letters. 3-Vowel and 6-Consonants. It's a transport " \
+                                         "used in space"
+                    elif tip == tip_click and random_password == 'star':
+                        lb_tip['text'] = "This password contains 4 letters. 1-Vowel and 3-Consonants. It's a large and " \
+                                         "luminous plasma sphere"
+
                 # GUI for level 1
+                # --------------------------- PASSWORD ATTEMPTS GUI ---------------------------
                 password = Entry(win)
                 password.place(x=350, y=250)
 
@@ -160,10 +78,27 @@ def start_click():
 
                 lb_attempt = Label(win, text='')
                 lb_attempt.place(x=350, y=350)
+                # --------------------------- END OF PASSWORD ATTEMPTS GUI ----------------------
 
-                lb_s = Label(win, text='')
-                lb_s.place(x=400, y=400)
+                # --------------------------- TIPS GUI -----------------------------
+                b_tip = Button(win, text='Click here for one password tip', command=tip_click)
+                b_tip.place(x=100, y=150)
 
+                lb_tip = Label(win, text='')
+                lb_tip.place(x=100, y=180)
+
+                # --------------------------- END OF TIPS GUI -----------------------
+
+                # --------------------------- POSSIBLE PASSWORDS GUI ---------------------------
+                b_possible_passwords = Button(win, text='Possible Passwords: ', command=possible_passwords_click)
+                b_possible_passwords.place(x=550, y=150)
+
+                lb_possible_passwords_list = Label(win, text='')
+                lb_possible_passwords_list.place(x=680, y=150)
+
+                # --------------------------- END OF POSSIBLE PASSWORDS GUI ---------------------
+
+        # --------------------------- LEVELS GUI ---------------------------
         b_level_1 = Button(win, text='Level 1', command=level1_click)
         b_level_1.place(x=300, y=150)
 
@@ -173,10 +108,98 @@ def start_click():
         b_level_3 = Button(win, text='Level 3')
         b_level_3.place(x=500, y=150)
 
+        # --------------------------- END OF LEVELS GUI --------------------
+        def level2_click():
+            level2 = level2_click
+            if level2 == level2_click:
+                b_level_1.destroy()
+                b_level_2.destroy()
+                b_level_3.destroy()
+                lp = ['meteorite', 'Milky Way']
 
-# General GUI
+                random_password = choice(lp)
+
+                def try_password_click():
+                    try_password = try_password_click
+                    p = str(password.get())
+                    if try_password == try_password_click:
+                        c_random_password = [char for char in lp]
+                        password_string = ''.join(map(str, c_random_password))  # Convert the ll list to string
+                        if random_password == 'meteorite':
+                            if p == 'meteorite':
+                                lb_attempt['text'] = 'Password Succesfully Cracked!'
+                            else:
+                                lb_attempt['text'] = 'Password Not Cracked!'
+
+                        if random_password == 'milky Way':
+                            if p == 'milky Way':
+                                lb_attempt['text'] = 'Password Succesfully Cracked!'
+                            else:
+                                lb_attempt['text'] = 'Password Not Cracked!'
+
+                def possible_passwords_click():
+                    possible_passwords = possible_passwords_click
+                    if possible_passwords == possible_passwords_click:
+                        lb_possible_passwords_list['text'] = 'meteorite \nmilky way'
+
+                def tip_click():
+                    tip = tip_click
+                    if tip == tip_click and random_password == 'meteorite':
+                        lb_tip['text'] = "This password contains 9 letters. 5-Vowels and 4-Consonants. It's formed by " \
+                                         "space garbage"
+                    elif tip == tip_click and random_password == 'milky way':
+                        lb_tip['text'] = "This password contains 8 letters. 4-Vowel and 4-Consonants. It has got a " \
+                                         "drink name. "
+                # GUI for level 1
+                # --------------------------- PASSWORD ATTEMPTS GUI ---------------------------
+                password = Entry(win)
+                password.place(x=350, y=250)
+
+                lb_password_attempt = Label(win, text='Password Attempt: ')
+                lb_password_attempt.place(x=200, y=250)
+
+                b_try_password = Button(win, text='TRY', command=try_password_click)
+                b_try_password.place(x=500, y=250)
+
+                lb_attempt = Label(win, text='')
+                lb_attempt.place(x=350, y=350)
+                # --------------------------- END OF PASSWORD ATTEMPTS GUI ----------------------
+
+                # --------------------------- TIPS GUI -----------------------------
+                b_tip = Button(win, text='Click here for one password tip', command=tip_click)
+                b_tip.place(x=100, y=150)
+
+                lb_tip = Label(win, text='')
+                lb_tip.place(x=100, y=180)
+
+                # --------------------------- END OF TIPS GUI -----------------------
+
+                # --------------------------- POSSIBLE PASSWORDS GUI ---------------------------
+                b_possible_passwords = Button(win, text='Possible Passwords: ', command=possible_passwords_click)
+                b_possible_passwords.place(x=550, y=150)
+
+                lb_possible_passwords_list = Label(win, text='')
+                lb_possible_passwords_list.place(x=680, y=150)
+
+                # --------------------------- END OF POSSIBLE PASSWORDS GUI ---------------------
+
+        # --------------------------- LEVELS GUI ---------------------------
+        b_level_1 = Button(win, text='Level 1', command=level1_click)
+        b_level_1.place(x=300, y=150)
+
+        b_level_2 = Button(win, text='Level 2')
+        b_level_2.place(x=400, y=150)
+
+        b_level_3 = Button(win, text='Level 3')
+        b_level_3.place(x=500, y=150)
+        # --------------------------- END OF LEVELS GUI --------------------
+
+
+# --------------------------- GENERAL GUI ---------------------------
 b_start = Button(win, text='Start', command=start_click)
-b_start.place(x=400, y=300)
+b_start.place(x=380, y=250)
 
 win.geometry('800x600')
 win.mainloop()
+# --------------------------- END OF GENERAL GUI ---------------------------
+
